f3dcad5430f43dedff4dd8dbf247c336723b5630
Varun <varunpandya0704@gmail.com>
2024-09-21 13:18:38 +0000

    first commit

---
+++ start.java
@@ -0,0 +1,17 @@
+public class start{
+    public static void main(String[] public static void main(String[] args) {
+        int salary = 25400;
+        if(salary>=10000){
+            salary = salary + 2000;
+        }
+        else if (salary > 5000 && salary<10000) {
+            salary = salary + 1000;
+        }
+
+        else{
+            salary = salary + 500
+        }
+    
+    System.out.println(salary);
+    }
+}
